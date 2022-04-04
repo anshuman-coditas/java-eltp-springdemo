acfbbb5338d562bc8966eee8f9d0d9089554fada
Anshuman Kumar <anshuman.kumar@coditas.com>
2022-04-04 15:47:49 +0530

    IC

---
+++ SpringDemo/src/main/java/com/HelloWorld.java
@@ -0,0 +1,11 @@
+package com;
+
+public class HelloWorld {
+    private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    void print(){
+        System.out.println("Spring first "+name);
+    }
+}
